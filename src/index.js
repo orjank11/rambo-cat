453f18cf1c9d34939eab6ebb891ab13165e3b1f4
orjank11 <orjank11@users.noreply.github.com>
2018-06-04 20:16:15 +0200

    Killer cat is done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ShootingScene from './ShootingScene';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<ShootingScene />, document.getElementById('root'));
