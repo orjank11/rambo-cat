4b594ea0f48c01e1c430e9d6ea58c9982404b52d
orjank11 <orjank11@users.noreply.github.com>
2018-06-26 16:08:36 +0200

    clean

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './App.css';
+import ShootingScene from './ShootingScene';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<ShootingScene />, document.getElementById('root'));
