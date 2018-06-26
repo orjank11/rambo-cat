d00480189dda08273f5804ba3c73ce078e6ed10f
orjank11 <orjank11@users.noreply.github.com>
2018-06-26 16:03:49 +0200

    default files

---
+++ src/ShootingScene.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Rambo from './Rambo';
+import Gun from './Gun';
+
+class SchootingScene extends Component {
+  render() {
+    return (
+      <div className="ShootingScene">
+        <div>
+          <Rambo />
+          <Gun />
+          {[...Array(50)].map((x, i) => {
+            return (
+              <span key={`bullet-${i}`} className={`bullet bullet-${i}`} />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SchootingScene;
